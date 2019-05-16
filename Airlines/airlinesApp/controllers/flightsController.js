f90802376d29c7abaceb590d8a8700da12dad94a
Brad Coleman <bcoleman@cantongroup.onmicrosoft.com>
2019-05-16 15:11:05 -0400

    commit prior to adding button select

--- Airlines/airlinesApp/controllers/flightsController.js
+++ Airlines/airlinesApp/controllers/flightsController.js
@@ -2,6 +2,17 @@
 
     var flightsController = function($scope) {
 
+
+        $scope.selectFlight = function (flight) {
+
+            for(var f in $scope.flights) {
+                f.approved = false;
+            }
+
+            flight.approved = !flight.approved;
+        };
+
+
         $scope.flights =
         [
             {
@@ -23,7 +34,28 @@
                 price: 1500,
                 cities: ["DUL", "LAG"],
                 approved: false
+            },
+            {
+                airline: "JetBlue",
+                flightNumber: 102,
+                startTime: "12:00",
+                endTime: "15:00",
+                totalDuration: 3,
+                price: 500,
+                cities: ["BWI", "LAX"],
+                approved: false
+            },
+            {
+                airline: "US Air",
+                flightNumber: 98,
+                startTime: "11:00",
+                endTime: "15:00",
+                totalDuration: 4,
+                price: 1500,
+                cities: ["DUL", "LAG"],
+                approved: false
             }
+
         ];
     }
 
